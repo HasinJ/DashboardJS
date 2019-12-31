80352a11169ebace99519c618fd9a27dc7f66dbb
HasinJ <hasinchoudhuryj@gmail.com>
2019-12-30 23:02:22 -0500

    Initial Commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+  //establishes connection to database using AWS
+  define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+  define('DB_USER', $_SERVER['RDS_USERNAME']);
+  define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+  define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+  define('DB_TABLE', 'storec');
+
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+
+include_once('connection.php'); //connection credentials
+
+//limit of queries 
+$limit = 20;
+
+//hello world
+$hi = "hello, here are the used data points: $limit";
+echo "$hi <br>" ;
+
+//how much data to display as echo, and initial set of data points for dynamic-ish chart
+
+
+
+//database connection
+$dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
+try
+{
+$pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
+
+//var_dump($pdo); // checks if PDO connection/object has been created
+}
+
+catch(Exception $e)
+{
+	echo "an error has occured";
+}
+
+//grabbing query from pdo connection
+$dataPoints = array();
+$labelTime = array();
+foreach($pdo->query("SELECT * FROM storec LIMIT " . $limit) as $row )
+{
+	echo $row["Date"] . " " . $row[dow] . " " . $row[Stonewall] . " " . $row[Foxchase] . " " . $row[Warrenton] . " " . $row[Bristow] . " " . $row[BJ] . " " . $row[Heritage] . " " . $row[Eastgate] . " " . "<br>";
+
+	array_push($dataPoints, $row[Foxchase]);
+	array_push($labelTime, $row["Date"]);
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class = "col-md-10 offset-md-1">
+      <h1 align = "center">FoxChase (second column)</h1>
+      <canvas id="myChart"></canvas>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: <?php echo json_encode($labelTime, JSON_NUMERIC_CHECK); ?>,
+        datasets: [{
+            label: 'Foxchase',
+			fill: false,
+            //backgroundColor: 'rgb(0, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
