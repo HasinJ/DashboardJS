381ed614ef2c26bafeee5aec208e8f12c46f210c
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-28 23:20:07 -0500

    added date input box with button that has event sensor that uses grabbed php"

--- index.php
+++ index.php
@@ -1,47 +1,17 @@
+
 <?php
-
-
-include_once('connection.php'); //connection credentials
-
-//limit of queries 
-$limit = 20;
-
-//hello world
-$hi = "hello, here are the used data points: $limit";
-echo "$hi <br>" ;
-
-//how much data to display as echo, and initial set of data points for dynamic-ish chart
-
-
-
-//database connection
-$dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
-try
-{
-$pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
-
-//var_dump($pdo); // checks if PDO connection/object has been created
-}
-
-catch(Exception $e)
-{
-	echo "an error has occured";
-}
-
 //grabbing query from pdo connection
 $dataPoints = array();
 $labelTime = array();
-foreach($pdo->query("SELECT * FROM storec LIMIT " . $limit) as $row )
-{
-	echo $row["Date"] . " " . $row[dow] . " " . $row[Stonewall] . " " . $row[Foxchase] . " " . $row[Warrenton] . " " . $row[Bristow] . " " . $row[BJ] . " " . $row[Heritage] . " " . $row[Eastgate] . " " . "<br>";
-
-	array_push($dataPoints, $row[Foxchase]);
-	array_push($labelTime, $row["Date"]);
+for ($i=0; $i < 10; $i++) {
+  array_push($dataPoints, $i);
 }
 
-
-
+for ($i=0; $i > -10 ; $i--) {
+  array_push($labelTime, $i);
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -51,13 +21,20 @@ foreach($pdo->query("SELECT * FROM storec LIMIT " . $limit) as $row )
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class = "col-md-10 offset-md-1">
-      <h1 align = "center">FoxChase (second column)</h1>
+
+    <div id="container">
+      <input type="date" name="date">
+      <input type="submit" value="submit date">
+    </div>
+
+    <div class="col-md-10 offset-md-1">
+      <h1 align="center" class="graphTitle"></h1>
       <canvas id="myChart"></canvas>
     </div>
 
@@ -65,26 +42,42 @@ foreach($pdo->query("SELECT * FROM storec LIMIT " . $limit) as $row )
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script>
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: <?php echo json_encode($labelTime, JSON_NUMERIC_CHECK); ?>,
-        datasets: [{
-            label: 'Foxchase',
-			fill: false,
-            //backgroundColor: 'rgb(0, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-        }]
-    },
+    let graphTitle = document.querySelector('.graphTitle');
+    graphTitle.textContent = 'Title';
 
-    // Configuration options go here
-    options: {}
-});
+    let inputs = document.getElementById('container').querySelectorAll('input');
+
+    inputs.forEach( (input) => {
+      input.addEventListener('click',(e) => {
+        if(e.target.type === 'submit') createChart();
+      });
+    });
+
+
+
+    function createChart() {
+      let ctx = document.getElementById('myChart').getContext('2d');
+      let chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+          labels: <?php echo json_encode($labelTime, JSON_NUMERIC_CHECK); ?>,
+          datasets: [{
+              label: 'Foxchase',
+  			fill: false,
+              //backgroundColor: 'rgb(0, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+          }]
+      },
+
+      // Configuration options go here
+      options: {}
+  });
+}
     </script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
