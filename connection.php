ffcd4e8f3d48d82239b0756c7e23389a11ecec1e
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-31 15:05:28 -0500

    added connect function to connection ( OOP php) instead of putting it to index

--- connection.php
+++ connection.php
@@ -7,4 +7,21 @@
   define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
   define('DB_TABLE', 'storec');
 
+  protected function connect() {
+    //database connection
+    $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
+
+    try {
+    $pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
+    //var_dump($pdo); // checks if PDO connection/object has been created
+    }
+
+    catch(Exception $e) {
+    	echo "an error has occured";
+    }
+
+    return $pdo;
+
+  }
+
 ?>
