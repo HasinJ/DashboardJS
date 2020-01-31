80a5d2373fe44d4d649b7ed377c39fdca024a94b
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-31 15:09:09 -0500

    added instructional comments

--- codeForSubmit.js
+++ codeForSubmit.js
@@ -8,3 +8,11 @@ inputs.forEach( (input) => {
     if(e.target.type === 'submit') createChart();
   });
 });
+
+/*
+first box has holidays
+second box has date
+third box has free text
+
+second form returns the info just inputted (but it's not planned, but rather pulled from the database)
+*/
