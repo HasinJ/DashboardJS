31e3abb3cfc3c9f868947ee65d2cda16c8dcac63
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-31 20:38:23 -0500

    added back some AWS specified coding just in case

--- connection.php
+++ connection.php
@@ -1,32 +1,32 @@
 <?php
 
-  protected $DB_NAME;
-  protected $DB_HOST;
-  protected $DB_USER;
-  protected $DB_PASSWORD;
-  protected $DBtable;
+  define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+  define('DB_USER', $_SERVER['RDS_USERNAME']);
+  define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+  define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+  define('DB_TABLE', 'storec');
 
-  protected function connectRDS() {
+  function connectRDS() {
     //database connection
 
-    $this->DB_NAME = 'hasindatabase';
-    $this->DB_HOST = 'hasindatabase.c0v7lriogf7u.us-east-2.rds.amazonaws.com';
-    $this->DB_USER = 'admin';
-    $this->DB_PASSWORD = 'hasinmc11';
-    $this->DBtable = 'storec';
+    $DB_NAME = 'hasindatabase';
+    $DB_HOST = 'hasindatabase.c0v7lriogf7u.us-east-2.rds.amazonaws.com';
+    $DB_USER = 'admin';
+    $DB_PASSWORD = 'Hasinmc11';
+    $DBtable = 'storec';
 
-    $dsn = 'mysql:dbname=' . $this->DB_NAME . ';host=' . $this->DB_HOST;
+    $dsn = 'mysql:host=' . $DB_HOST . ';dbname=' . $DB_NAME . ';charset=utf8mb4' . ';port=3306';
 
     try {
-    $pdo = new PDO($dsn, $this->DB_USER, $this->DB_PASSWORD);
+    $pdo = new PDO($dsn, $DB_USER, $DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
     //var_dump($pdo); // checks if PDO connection/object has been created
     }
 
-    catch(Exception $e) {
-    	echo "an error has occured with DB connection";
+    catch(PDOException $e) {
+    	echo "an error has occured with DB connection ".$e->getMessage();
     }
-
-    return $pdo;
   }
 
 
