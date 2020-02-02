d8bcd22a2a0688c7fd61ad090b634918aacc5f41
HasinJ <hasinchoudhuryj@gmail.com>
2020-02-02 02:17:59 -0500

    grabs user inputted date and holiday to grab multiple fields of query and stash it into an array + exception handling

--- forms/retrieve.php
+++ forms/retrieve.php
@@ -8,10 +8,29 @@
   try {
     $date = $_POST['date'];
     $holiday = $_POST['holiday'];
-    $pdo->exec("SELECT inputDate, holiday, freetext FROM submitTEST WHERE holiday = '$holiday';")
+
+    $sql = "SELECT inputDate, holiday, freetext FROM submitTEST WHERE inputDate = :inputDate AND holiday = :holiday";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute(array('inputDate' => $date, 'holiday' => $holiday)); //same as bind
+    /*$stmt->bindParam(':holiday', $holiday);
+    $stmt->execute();*/
+    $results = array();
+    $results = ($stmt->fetch(PDO::FETCH_ASSOC));
+    if ($results == NULL) {
+      throw new Exception();
+    }
+    foreach ($results as $result) {
+      echo $result;
+      echo '<br>';
+    }
+
   } catch (PDOException $e) {
     echo $e->getMessage();
+  } catch (Exception $e) {
+    echo "nothing for this holiday";
   }
+
 
 
 ?>
