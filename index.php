da63ee48e48c164e04df174b727cac0345e48b82
HasinJ <hasinchoudhuryj@gmail.com>
2020-02-02 00:01:29 -0500

    adjusted code for switched js files, adjusted code for OOP integration, created HTML elements for database input

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 
 <?php
 
-include_once('connection.php');
+require('connection.php');
 
-$pdo = connectRDS();
+$connObject = new db;
+
+$pdo = $connObject->connectLOCAL();
+//$pdo = $connobject->connectRDS();
+
+/*
 $limit = 20;
 
 //grabbing query from pdo connection (dummy data for now)
@@ -15,8 +20,10 @@ foreach($pdo->query("SELECT * FROM storec LIMIT " . $limit) as $row )
 	array_push($dataPoints, $row['Foxchase']);
 	array_push($labelTime, $row["Date"]);
 }
+*/
 
-/*
+$dataPoints = array();
+$labelTime = array();
 for ($i=0; $i < 10; $i++) {
   array_push($dataPoints, $i);
 }
@@ -24,7 +31,7 @@ for ($i=0; $i < 10; $i++) {
 for ($i=0; $i > -10 ; $i--) {
   array_push($labelTime, $i);
 }
-*/
+
 ?>
 
 
@@ -36,17 +43,46 @@ for ($i=0; $i > -10 ; $i--) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="main.css">
 
-    <title>Hello, world!</title>
+    <title></title>
   </head>
   <body>
+		<div class="col-md-12">
 
-    <div id="container">
-      <input type="date" name="date">
-      <input type="submit" value="submit date">
-    </div>
+	    <div class="col-md-6" style="margin-top:20px;">
+				<form id="putIn" action="forms/action.php" method="post">
+
+						<input type="date" name="date">
+						<select name="holiday">
+							<option value="">Select Holiday</option>
+							<option value="Christmas">Christmas</option>
+							<option value="Valentines">Valentines</option>
+							<option value="Easter">Easter</option>
+						</select><br>
+						<textarea style="margin-top:20px;" name="freetext" rows="1" cols="25"></textarea><br>
+						<input type="submit" value="submit">
+
+				</form>
+			</div>
+
+			<div class="col-md-6" style="margin-top:40px;">
+				<form id="pullOut" action="/forms/retrieve.php" method="post">
+
+					<input type="date" name="date">
+					<select name="holiday">
+						<option value="">Select Holiday</option>
+						<option value="Christmas">Christmas</option>
+						<option value="Valentines">Valentines</option>
+						<option value="Easter">Easter</option>
+					</select><br>
+					<input type="submit" value="retrieve">
+
+				</form>
+			</div>
+
+		</div>
 
     <div class="col-md-10 offset-md-1">
       <h1 align="center" class="graphTitle"></h1>
@@ -56,10 +92,12 @@ for ($i=0; $i > -10 ; $i--) {
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script src="codeForSubmit.js"></script>
+    <script src="js/codeForSubmit.js"></script>
 
 
     <script>
+		createChart();
+
     function createChart() {
       let ctx = document.getElementById('myChart').getContext('2d');
       let chart = new Chart(ctx, {
