18fe64a6774aa14a8912749a4f3880b903f86622
HasinJ <hasinchoudhuryj@gmail.com>
2020-02-01 23:57:49 -0500

    added forms folder to hold forms that deal with retrieving and inputting data into database using pdo

---
+++ forms/action.php
@@ -0,0 +1,40 @@
+<?php
+
+  require('../connection.php');
+  $connection = new db;
+  $pdo = $connection->connectLOCAL();
+
+
+  /* this is for creating the table if it doesn't exist
+  $sql = 'CREATE TABLE submitTEST(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    inputDate DATE,
+    holiday VARCHAR(30) NOT NULL,
+    freetext TEXT(1000)
+  )';
+
+  try {
+    $pdo->exec($sql);
+  }
+  catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+  */
+
+try {
+
+  $date = $_POST['date'];
+  $holiday = $_POST['holiday'];
+  $freetext = $_POST['freetext'];
+  $pdo->exec("INSERT INTO submitTEST (inputDate, holiday, freetext)
+  VALUES ('$date', '$holiday', '$freetext')");
+
+  echo "submitted successfully!";
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
+
+
+
+?>
