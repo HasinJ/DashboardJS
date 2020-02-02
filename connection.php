46d6eb4fc4176dc3cf12d315477e152d6dc1b252
HasinJ <hasinchoudhuryj@gmail.com>
2020-02-01 23:59:31 -0500

    added OOP php and LOCALHOST database connection (kept RDS connection)

--- connection.php
+++ connection.php
@@ -6,28 +6,63 @@
   define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
   define('DB_TABLE', 'storec');
 */
-  function connectRDS() {
-    //database connection
 
-    $DB_NAME = 'hasindatabase';
-    $DB_HOST = 'hasindatabase.c0v7lriogf7u.us-east-2.rds.amazonaws.com';
-    $DB_USER = 'admin';
-    $DB_PASSWORD = 'hasinmc11';
-    $DBtable = 'storec';
 
-    $dsn = 'mysql:host=' . $DB_HOST . ';dbname=' . $DB_NAME . ';charset=utf8mb4' . ';port=3306';
+class db
+{
+
+  private $DB_NAME;
+  private $DB_HOST;
+  private $DB_USER;
+  private $DB_PASSWORD;
+  private $DBtable;
+
+  private function pdo(){
+    $dsn = 'mysql:host=' . $this->DB_HOST . ';dbname=' . $this->DB_NAME . ';charset=utf8mb4' . ';port=3306';
 
     try {
-    $pdo = new PDO($dsn, $DB_USER, $DB_PASSWORD);
+    $pdo = new PDO($dsn, $this->DB_USER, $this->DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
     //var_dump($pdo); // checks if PDO connection/object has been created
     }
 
     catch(PDOException $e) {
-    	echo "an error has occured with DB connection ".$e->getMessage();
+      echo "an error has occured with DB connection ".$e->getMessage();
     }
   }
+
+  public function connectRDS() {
+
+    $this->DB_NAME = 'hasindatabase';
+    $this->DB_HOST = 'hasindatabase.c0v7lriogf7u.us-east-2.rds.amazonaws.com';
+    $this->DB_USER = 'admin';
+    $this->DB_PASSWORD = 'hasinmc11';
+    $this->DBtable = 'storec';
+
+    return $this->pdo();
+
+  }
+
+  public function connectLOCAL() {
+
+    $this->DB_NAME = 'test';
+    $this->DB_HOST = 'localhost';
+    $this->DB_USER = 'root';
+    $this->DB_PASSWORD = '';
+    $this->DBtable = '';
+
+    return $this->pdo();
+
+  }
+
+  public function getTable() {
+    return $this->$DBtable;
+  }
+}
+
+
+
 
 
 ?>
