e68ae3bc326b60eff797a069ee6687bc5163d4b2
HasinJ <hasinchoudhuryj@gmail.com>
2020-02-09 20:54:55 -0500

    added another get method to graph OOP

--- phpClasses/graph.php
+++ phpClasses/graph.php
@@ -8,26 +8,25 @@ class graph
     private $dataPoints = array();
     private $labelTime = array();
     private $table = 'beverages';
+    private $column = '';
     private $pdo; //obj
 
     public function __construct($pdo)
-    {
-      $this->pdo = $pdo;
-    }
+    { $this->pdo = $pdo;}
 
     public function fillLine($column){
       $this->dataPoints = array();
       $this->labelTime = array();
-      
+      $this->column = $column;
+
       try {
         foreach($this->pdo->query("SELECT * FROM " . $this->table . " LIMIT " . $this->limit) as $row )
         {
         	array_push($this->labelTime, $row["Date"]);
         	array_push($this->dataPoints, $row[$column]);
-
         }
-
       }
+
       catch (PDOException $e) {
         echo 'error with graph: ' . $e->getMessage();
       }
@@ -39,6 +38,10 @@ class graph
     public function getLabelTime(){
       return $this->labelTime;
     }
+    public function getColumn(){
+      return $this->column;
+    }
+    
     public function setColumn($column){
       $this->column = $column;
     }
